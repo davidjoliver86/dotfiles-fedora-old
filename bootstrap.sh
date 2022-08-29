4d16332141695283b9c562d62393593294e088da
davidjoliver86 <davidjoliver.2021@gmail.com>
2022-08-28 20:00:43 -0700

    gnome setup; update vscode config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-dnf install -y git
+dnf install -y git python3-psutil
 /usr/bin/python3 -m venv /usr/lib/virtualenvs/ansible --system-site-packages
 
 /usr/lib/virtualenvs/ansible/bin/pip install --upgrade pip
-/usr/lib/virtualenvs/ansible/bin/pip install 'ansible<6'
+/usr/lib/virtualenvs/ansible/bin/pip install ansible
 
 mkdir -p /etc/ansible
 cat <<EOF > /etc/ansible/hosts
