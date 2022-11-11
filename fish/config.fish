8dc839bb2e38aa69b2b5417cc174d66bac6c43fe
davidjoliver86 <davidjoliver.2021@gmail.com>
2022-11-11 11:19:52 -0800

    disable omf; use starship

--- fish/config.fish
+++ fish/config.fish
@@ -4,34 +4,6 @@ fish_vi_key_bindings
 # let theme handle virtualenvs
 set -xg VIRTUAL_ENV_DISABLE_PROMPT 1
 
-# colors
-set theme_color_scheme zenburn
-set -U fish_color_normal normal
-set -U fish_color_command c397d8
-set -U fish_color_quote b9ca4a
-set -U fish_color_redirection 70c0b1
-set -U fish_color_end c397d8
-set -U fish_color_error d54e53
-set -U fish_color_param 7aa6da
-set -U fish_color_comment e7c547
-set -U fish_color_match --background=brblue
-set -U fish_color_selection white --bold --background=brblack
-set -U fish_color_search_match bryellow --background=brblack
-set -U fish_color_history_current --bold
-set -U fish_color_operator 00a6b2
-set -U fish_color_escape 00a6b2
-set -U fish_color_cwd green
-set -U fish_color_cwd_root red
-set -U fish_color_valid_path --underline
-set -U fish_color_autosuggestion 969896
-set -U fish_color_user brgreen
-set -U fish_color_host normal
-set -U fish_color_cancel -r
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description B3A06D yellow
-set -U fish_pager_color_prefix white --bold --underline
-set -U fish_pager_color_progress brwhite --background=cyan
-
 # editor
 set -xg EDITOR vim
 
@@ -44,3 +16,9 @@ end
 
 # no blinking cursor
 set fish_cursor_unknown block
+
+# aws autocomplete
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+
+# starship
+starship init fish | source
--- omf/before.init.fish
+++
@@ -1,2 +0,0 @@
-# add a symlink to aws_completion's pyenv shim in this folder
-set PATH $HOME/.bin/ $PATH
