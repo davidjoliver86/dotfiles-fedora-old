b6fe67592c2151a4ffcd797a16df68004a0ea390
davidjoliver86 <davidjoliver.2021@gmail.com>
2022-11-13 10:53:08 -0800

    moar randomness

--- fish/config.fish
+++ fish/config.fish
@@ -20,5 +20,44 @@ set fish_cursor_unknown block
 # aws autocomplete
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
+# theme
+set -U fish_color_normal normal
+set -U fish_color_command c397d8
+set -U fish_color_quote b9ca4a
+set -U fish_color_redirection 70c0b1
+set -U fish_color_end c397d8
+set -U fish_color_error d54e53
+set -U fish_color_param 7aa6da
+set -U fish_color_comment e7c547
+set -U fish_color_match --background=brblue
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 969896
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel --reverse
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D
+set -U fish_pager_color_selected_background --background=brblack
+set -U fish_pager_color_selected_prefix
+set -U fish_pager_color_background
+set -U fish_pager_color_secondary_prefix
+set -U fish_pager_color_secondary_description
+set -U fish_pager_color_secondary_background
+set -U fish_pager_color_selected_completion
+set -U fish_color_host_remote
+set -U fish_color_keyword
+set -U fish_pager_color_selected_description
+set -U fish_color_option
+set -U fish_pager_color_secondary_completion
+
 # starship
 starship init fish | source
